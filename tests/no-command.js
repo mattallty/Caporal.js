9bdfe1da9b7f134a631e42795d148d0210568db9
Matthias ETIENNE <matt@allty.com>
2017-02-19 18:49:03 +0100

    test: added tests
    added test for argument()

--- tests/no-command.js
+++ tests/no-command.js
@@ -2,19 +2,38 @@
 
 /* global Program, logger, should, makeArgv, sinon */
 
-const program = new Program();
 
-program
-  .logger(logger)
-  .version('1.0.0')
-  .reset();
 
 describe('Setting up no command() but an action()', () => {
+
+  const program = new Program();
+
+  program
+    .logger(logger)
+    .version('1.0.0');
+
   it(`should execute action()`, (done) => {
     program.action(() => {
       done();
     });
     program.parse([]);
+  });
+});
+
+describe('Setting up no command() but an argument() and an action()', () => {
+
+  const program = new Program();
+
+  program
+    .logger(logger)
+    .version('1.0.0')
+    .argument('<foo>', 'My foo arg');
+
+  it(`should execute action()`, (done) => {
+    program.action(() => {
+      done();
+    });
+    program.parse(makeArgv(['myarg']));
   });
 });
 
