20015a3d6d3b3293354affc2d8a70cd15026ddb6
Matthias ETIENNE <matt@allty.com>
2017-02-19 23:18:01 +0100

    fix: fix release script
    fix release script and add npm deps as local dev deps

--- release.sh
+++ release.sh
@@ -13,24 +13,24 @@ npm install -g trash conventional-recommended-bump conventional-changelog conven
 
 np() {
     travis status --no-interactive &&
-    trash node_modules &>/dev/null;
+    ./node_modules/.bin/trash node_modules &>/dev/null;
     git pull --rebase &&
     npm install &&
     npm test &&
     cp package.json _package.json &&
-    preset=`conventional-commits-detector` &&
+    preset=`./node_modules/.bin/conventional-commits-detector` &&
     echo $preset &&
-    bump=`conventional-recommended-bump -p angular` &&
+    bump=`./node_modules/.bin/conventional-recommended-bump -p angular` &&
     echo ${1:-$bump} &&
     npm --no-git-tag-version version ${1:-$bump} &>/dev/null &&
-    conventional-changelog -i CHANGELOG.md -s -p ${2:-$preset} &&
+    ./node_modules/.bin/conventional-changelog -i CHANGELOG.md -s -p ${2:-$preset} &&
     git add CHANGELOG.md &&
-    version=`cat package.json | json version` &&
+    version=`cat package.json | ./node_modules/.bin/json version` &&
     git commit -m"docs(CHANGELOG): $version" &&
     mv -f _package.json package.json &&
     npm version ${1:-$bump} -m "chore(release): %s" &&
     git push --follow-tags &&
-    conventional-github-releaser -p ${2:-$preset} &&
+    ./node_modules/.bin/conventional-github-releaser -p ${2:-$preset} &&
     npm publish
 }
 
