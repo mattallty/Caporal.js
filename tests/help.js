9ed4683eab61057410246a47c117e9dcc8f6c5a2
Matthias ETIENNE <matt@allty.com>
2017-04-12 11:05:02 +0200

    feat(help): Added custom help feature
    Allow user to define a custom help for both global and commands (fixes
    #30)
    Also fixes #14.

--- tests/help.js
+++ tests/help.js
@@ -18,7 +18,7 @@ describe('Calling {program} help', function() {
       .option('-f, --foo', 'Foo option')
       .action(function() {});
 
-    const help = sinon.spy(program, "help");
+    const help = sinon.spy(program, "_help");
     const exit = sinon.stub(process, "exit");
     program.parse(makeArgv('help'));
     should(help.callCount).be.eql(1);
@@ -43,7 +43,7 @@ describe('Calling {program} help', function() {
       .action(function() {})
 
 
-    const help = sinon.spy(program, "help");
+    const help = sinon.spy(program, "_help");
     const exit = sinon.stub(process, "exit");
     program.parse(makeArgv('help'));
     should(help.callCount).be.eql(1);
@@ -70,7 +70,7 @@ describe('Calling {program} help', function() {
       .action(function() {})
 
 
-    const help = sinon.spy(program, "help");
+    const help = sinon.spy(program, "_help");
     const exit = sinon.stub(process, "exit");
     program.parse(makeArgv(['help', 'command1']));
     should(help.callCount).be.eql(1);
@@ -79,6 +79,7 @@ describe('Calling {program} help', function() {
     help.restore();
     program.reset();
   });
+
 
   it(`should output command-specific help for single command program`, function() {
     program
@@ -90,11 +91,338 @@ describe('Calling {program} help', function() {
       .option('-b, --bar', 'Bar option', null, 1, true)
       .action(function() {});
 
-    const help = sinon.spy(program, "help");
+    const help = sinon.spy(program, "_help");
     const exit = sinon.stub(process, "exit");
     program.parse(makeArgv(['help', 'command1']));
     should(help.callCount).be.eql(1);
     should(exit.callCount).be.eql(1);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+
+  it(`should output custom global help for single command program`, function() {
+    const customHelp = 'CUSTOM_HELP';
+    program
+      .help(customHelp)
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv('help'));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    should(program._help()).containEql(customHelp);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+  it(`should output specific command help for a program`, function() {
+    const customHelp = 'CUSTOM_HELP';
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .help(customHelp)
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['help', 'command1']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    should(program._help()).containEql(customHelp);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+});
+
+
+
+
+describe('Calling {program} --help', function() {
+
+  it(`should output global help for single command program`, function() {
+    program
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['--help']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+  it(`should output global help for multiple commands program`, function() {
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .action(function() {})
+      .command('command2', '2nd command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .action(function() {})
+
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['--help']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+  it(`should output command-specific help for multiple commands program`, function() {
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {})
+      .command('command2', '2nd command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {})
+
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['--help', 'command1']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    exit.restore();
+    help.restore();
+    program.reset();
+  });
+
+
+  it(`should output command-specific help for single command program`, function() {
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['--help', 'command1']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+
+  it(`should output custom global help for single command program`, function() {
+    const customHelp = 'CUSTOM_HELP';
+    program
+      .help(customHelp)
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['--help']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    should(program._help()).containEql(customHelp);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+  it(`should output specific command help for a program`, function() {
+    const customHelp = 'CUSTOM_HELP';
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .help(customHelp)
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['--help', 'command1']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    should(program._help()).containEql(customHelp);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+});
+
+
+
+
+
+describe('Calling {program} -h', function() {
+
+  it(`should output global help for single command program`, function() {
+    program
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['-h']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+  it(`should output global help for multiple commands program`, function() {
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .action(function() {})
+      .command('command2', '2nd command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .action(function() {})
+
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['-h']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+  it(`should output command-specific help for multiple commands program`, function() {
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {})
+      .command('command2', '2nd command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {})
+
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['-h', 'command1']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    exit.restore();
+    help.restore();
+    program.reset();
+  });
+
+
+  it(`should output command-specific help for single command program`, function() {
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['-h', 'command1']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+
+  it(`should output custom global help for single command program`, function() {
+    const customHelp = 'CUSTOM_HELP';
+    program
+      .help(customHelp)
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg')
+      .option('-f, --foo', 'Foo option')
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['-h']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    should(program._help()).containEql(customHelp);
+    help.restore();
+    exit.restore();
+    program.reset();
+  });
+
+  it(`should output specific command help for a program`, function() {
+    const customHelp = 'CUSTOM_HELP';
+    program
+      .description('my desc')
+      .command('command1', '1st command')
+      .help(customHelp)
+      .argument('<required>', 'Required arg')
+      .argument('[optional]', 'Optional arg', null, 2)
+      .option('-f, --foo', 'Foo option')
+      .option('-b, --bar', 'Bar option', null, 1, true)
+      .action(function() {});
+
+    const help = sinon.spy(program, "_help");
+    const exit = sinon.stub(process, "exit");
+    program.parse(makeArgv(['-h', 'command1']));
+    should(help.callCount).be.eql(1);
+    should(exit.callCount).be.eql(1);
+    should(program._help()).containEql(customHelp);
     help.restore();
     exit.restore();
     program.reset();
