2f9e5789b8d12a13cafc67598920940337eb6e0b
kinok <minthilith@gmail.com>
2017-06-30 11:44:45 +0200

    feat(logger): allow log level to be passed through env (#58)

--- tests/verbosity-default.js
+++ tests/verbosity-default.js
@@ -1,32 +1,62 @@
 "use strict";
 
-/* global Program, logger, should, makeArgv */
-
-const program = new Program();
-
-program
-  .logger(logger)
-  .version('1.0.0')
-  .reset()
-  .command('foo', 'My foo')
-  .action(function() {
-    this.debug('debug should NOT be displayed');
-    this.info("This should be displayed");
-    this.warn('This should be displayed');
-  });
+/* global Program, should, makeArgv */
+const createLogger = require('../lib/logger').createLogger;
 
 describe('Caporal with default verbosity', () => {
-  it(`should output at info level`, (done) => {
+  it(`should output at info level when CAPORAL_LOGGER_LEVEL is not set`, (done) => {
+    delete process.env.CAPORAL_LOGGER_LEVEL;
+    const program = new Program();
+    const customLogger = createLogger();
+
+    program
+      .logger(customLogger)
+      .version('1.0.0')
+      .reset()
+      .command('foo', 'My foo')
+      .action(function() {
+        this.debug('debug should NOT be displayed');
+        this.info("This should be displayed");
+        this.warn('This should be displayed');
+      });
 
     let output = 0;
     const listener = _ => output++;
-    logger.on('logging', listener);
+    customLogger.on('logging', listener);
 
     program.parse(makeArgv(['foo']));
 
     setImmediate(() => {
       should(output).eql(2);
-      logger.removeListener('logging', listener);
+      customLogger.removeListener('logging', listener);
+      done();
+    })
+  });
+
+  it(`should output at debug level when CAPORAL_LOGGER_LEVEL is set to debug`, (done) => {
+    process.env.CAPORAL_LOGGER_LEVEL = 'debug';
+    const program = new Program();
+    const customLogger = createLogger();
+    program
+      .logger(customLogger)
+      .version('1.0.0')
+      .reset()
+      .command('foo', 'My foo')
+      .action(function() {
+        this.debug('debug should be displayed');
+        this.info("This should be displayed");
+        this.warn('This should be displayed');
+      });
+
+    let output = 0;
+    const listener = _ => output++;
+    customLogger.on('logging', listener);
+
+    program.parse(makeArgv(['foo']));
+
+    setImmediate(() => {
+      should(output).eql(3);
+      customLogger.removeListener('logging', listener);
       done();
     })
   });
