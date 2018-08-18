0a9558f72a8b71048eb6b33490543f1572f9fd16
kinok <minthilith@gmail.com>
2018-08-18 14:41:34 +0000

    Fix error label when only short name option is provided (#122)
    * fix(options): Fixes #118 - Allow to only provide short name & have the correct error returned

---
+++ tests/issues/issue-118-only-shortname-options.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/* global Program, logger, should, makeArgv, sinon */
+
+const program = new Program();
+const chalk = require('chalk');
+
+program
+  .logger(logger)
+  .version('1.0.0')
+  .command('issue118', 'Fix!');
+
+
+describe('Issue #118 - Unknown option --undefined', () => {
+  it.only('should return shortname in error text whenever longname is not provided', () => {
+
+    sinon.stub(program, 'fatalError', (err) => {
+      should(err.name).eql('MissingOptionError');
+      should(err.originalMessage).equal(`Missing option ${chalk.italic('-z')}.`);
+    });
+    program.option('-z <whatever>', 'Random option', program.INT, null, true);
+    program.parse(makeArgv([]));
+
+  });
+});
