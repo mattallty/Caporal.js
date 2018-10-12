801f6673a3814988af363c908e340f6d325c6cd9
Matthias ETIENNE <matt@allty.com>
2018-10-12 23:02:17 +0200

    fix: fix it.only(...) causing the #118 issue test case to run alone (#124)

--- tests/issues/issue-118-only-shortname-options.js
+++ tests/issues/issue-118-only-shortname-options.js
@@ -12,7 +12,7 @@ program
 
 
 describe('Issue #118 - Unknown option --undefined', () => {
-  it.only('should return shortname in error text whenever longname is not provided', () => {
+  it('should return shortname in error text whenever longname is not provided', () => {
 
     sinon.stub(program, 'fatalError', (err) => {
       should(err.name).eql('MissingOptionError');
