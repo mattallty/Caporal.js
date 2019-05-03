d3926dd5e6c2ae40d916e15886535769be5f8037
Matthias Etienne <matt@allty.com>
2019-05-03 23:37:58 +0200

    fix(autocomplete): Fixes autocompletion of arguments

--- tests/autocomplete.js
+++ tests/autocomplete.js
@@ -21,13 +21,13 @@ describe('Autocomplete', () => {
       .argument('<kind>', 'Kind of pizza', ["margherita", "hawaiian", "fredo"])
       .argument('<from-store>', 'Which store to order from')
       // enable auto-completion for <from-store> argument using a sync function returning an array
-      .complete(function() {
+      .complete(function () {
         return ['store-1', 'store-2', 'store-3', 'store-4', 'store-5'];
       })
 
       .argument('<account>', 'Which account id to use')
       // enable auto-completion for <account> argument using a Promise
-      .complete(function() {
+      .complete(function () {
         return Promise.resolve(['account-1', 'account-2']);
       })
 
@@ -35,13 +35,13 @@ describe('Autocomplete', () => {
       .option('-d, --discount <amount>', 'Discount offer', this.program.FLOAT)
       .option('-p, --pay-by <mean>', 'Pay by option')
       // enable auto-completion for -p | --pay-by argument using a Promise
-      .complete(function() {
+      .complete(function () {
         return Promise.resolve(['cash', 'credit-card']);
       })
 
       // --extra will be auto-magicaly autocompleted by providing the user with 3 choices
       .option('-e <ingredients>', 'Add extra ingredients', ['pepperoni', 'onion', 'cheese'])
-      .action(function(args, options, logger) {
+      .action(function (args, options, logger) {
 
       })
 
@@ -50,16 +50,16 @@ describe('Autocomplete', () => {
       // <kind> will be auto-magicaly autocompleted by providing the user with 3 choices
       .argument('<order-id>', 'Order id')
       // enable auto-completion for <from-store> argument using the `done` callback
-      .complete(function() {
+      .complete(function () {
         return Promise.resolve(['#82792', '#71727', '#526Z52']);
       })
       .argument('<to-store>', 'Store id')
       .option('--ask-change <other-kind-pizza>', 'Ask for other kind of pizza')
-      .complete(function() {
+      .complete(function () {
         return Promise.resolve(["margherita", "hawaiian", "fredo"]);
       })
       .option('--say-something <something>', 'Say something to the manager')
-      .action(function(args, options, logger) {
+      .action(function (args, options, logger) {
 
       });
 
@@ -71,7 +71,7 @@ describe('Autocomplete', () => {
     this._complete.restore();
   });
 
-  it(`should complete commands when none provided`, function(done) {
+  it(`should complete commands when none provided`, function (done) {
 
     process.env.COMP_LINE = "_mocha";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -91,7 +91,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should complete commands when substring provided`, function(done) {
+  it(`should complete commands when substring provided`, function (done) {
 
     process.env.COMP_LINE = "_mocha or";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -113,7 +113,7 @@ describe('Autocomplete', () => {
   });
 
 
-  it(`should complete commands when substring (alias) provided`, function(done) {
+  it(`should complete commands when substring (alias) provided`, function (done) {
 
     process.env.COMP_LINE = "_mocha gi";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -132,7 +132,32 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should not complete commands if none available after string`, function(done) {
+  it.only(`should complete argument when substring provided`, function (done) {
+
+    process.env.COMP_LINE = "_mocha order mar";
+    process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
+    process.env.COMP_CWORD = process.env.COMP_LINE.trim().split(' ').length.toString();
+
+    this.program.parse(['node', '_mocha', 'completion', '--'].concat(process.env.COMP_LINE.split(' ')));
+
+    setImmediate(() => {
+      should(this._complete.called).be.true();
+      const response = this._complete.returnValues[0];
+      should(response).be.Promise();
+      should(response)
+        .be.fulfilledWith([
+          'margherita:Value for argument <kind>',
+          '--number:Number of pizza',
+          '--discount:Discount offer',
+          '--pay-by:Pay by option',
+          '-e:Add extra ingredients'
+        ])
+        .then(_ => done()).catch(err => done(err));
+
+    });
+  });
+
+  it(`should not complete commands if none available after string`, function (done) {
 
     process.env.COMP_LINE = "_mocha order";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -159,7 +184,7 @@ describe('Autocomplete', () => {
 
   });
 
-  it(`should not suggest argument(s) already provided`, function(done) {
+  it(`should not suggest argument(s) already provided`, function (done) {
 
     process.env.COMP_LINE = "_mocha order margherita";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -189,7 +214,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should not complete current command`, function(done) {
+  it(`should not complete current command`, function (done) {
 
     process.env.COMP_LINE = "_mocha order";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -218,7 +243,7 @@ describe('Autocomplete', () => {
 
   });
 
-  it(`should handle command alias`, function(done) {
+  it(`should handle command alias`, function (done) {
 
     process.env.COMP_LINE = "_mocha give-it-to-me";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -246,7 +271,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should suggest long option names`, function(done) {
+  it(`should suggest long option names`, function (done) {
 
     process.env.COMP_LINE = "_mocha order --n";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -269,7 +294,7 @@ describe('Autocomplete', () => {
 
   });
 
-  it(`should suggest long option value`, function(done) {
+  it(`should suggest long option value`, function (done) {
 
     process.env.COMP_LINE = "_mocha order -e";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -289,7 +314,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should work with _default command`, function(done) {
+  it(`should work with _default command`, function (done) {
 
     this.program = new Program();
 
@@ -301,13 +326,13 @@ describe('Autocomplete', () => {
       .argument('<kind>', 'Kind of pizza', ["margherita", "hawaiian", "fredo"])
       .argument('<from-store>', 'Which store to order from')
       // enable auto-completion for <from-store> argument using the `done` callback
-      .complete(function() {
+      .complete(function () {
         return ['store-1', 'store-2', 'store-3', 'store-4', 'store-5'];
       })
 
       .argument('<account>', 'Which account id to use')
       // enable auto-completion for <account> argument using a Promise
-      .complete(function() {
+      .complete(function () {
         return Promise.resolve(['account-1', 'account-2']);
       })
 
@@ -315,13 +340,13 @@ describe('Autocomplete', () => {
       .option('-d, --discount <amount>', 'Discount offer', this.program.FLOAT)
       .option('-p, --pay-by <mean>', 'Pay by option')
       // enable auto-completion for -p | --pay-by argument using a Promise
-      .complete(function() {
+      .complete(function () {
         return Promise.resolve(['cash', 'credit-card']);
       })
 
       // --extra will be auto-magicaly autocompleted by providing the user with 3 choices
       .option('-e <ingredients>', 'Add extra ingredients', ['pepperoni', 'onion', 'cheese'])
-      .action(function(args, options, logger) {
+      .action(function (args, options, logger) {
 
       });
 
@@ -345,7 +370,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should not suggest option names if last partial does not match`, function(done) {
+  it(`should not suggest option names if last partial does not match`, function (done) {
 
     process.env.COMP_LINE = "_mocha order --foo";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -365,7 +390,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should work with simple completers returning an array`, function(done) {
+  it(`should work with simple completers returning an array`, function (done) {
 
     process.env.COMP_LINE = "_mocha return";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -391,7 +416,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should handle arguments without completers`, function(done) {
+  it(`should handle arguments without completers`, function (done) {
 
     process.env.COMP_LINE = "_mocha return #82792";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -415,7 +440,7 @@ describe('Autocomplete', () => {
   });
 
 
-  it(`should handle options completers`, function(done) {
+  it(`should handle options completers`, function (done) {
 
     process.env.COMP_LINE = "_mocha return --ask-change";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -439,7 +464,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should handle options without completers`, function(done) {
+  it(`should handle options without completers`, function (done) {
 
     process.env.COMP_LINE = "_mocha return --say-something";
     process.env.COMP_POINT = process.env.COMP_LINE.length.toString();
@@ -459,7 +484,7 @@ describe('Autocomplete', () => {
     });
   });
 
-  it(`should handle a failling completion promise`, function(done) {
+  it(`should handle a failling completion promise`, function (done) {
 
     this.program = new Program();
 
@@ -467,10 +492,10 @@ describe('Autocomplete', () => {
       .logger(logger)
       .version('1.0.0')
       .argument('<from-store>', 'Which store to order from')
-      .complete(function() {
+      .complete(function () {
         return Promise.reject(new Error("foo"));
       })
-      .action(function(args, options, logger) {
+      .action(function (args, options, logger) {
 
       });
 
