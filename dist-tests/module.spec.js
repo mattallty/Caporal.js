8ccda3b04603922210abdc2bed09eb12ec5e45ee
Matthias Etienne <matt@allty.com>
2020-05-18 16:42:05 +0200

    build: fix dist tests

--- dist-tests/module.spec.js
+++ dist-tests/module.spec.js
@@ -1,14 +1,10 @@
-const { Program: ProgramClass } = require("../dist/program")
 const { default: prog, program, Program } = require("../dist")
 
 describe("Javascript module", () => {
   test("default export should be a Program instance", () => {
-    expect(prog).toBeInstanceOf(ProgramClass)
+    expect(prog).toBeInstanceOf(Program)
   })
   test("exports.program should be a Program instance", () => {
-    expect(program).toBeInstanceOf(ProgramClass)
-  })
-  test("exports.Program should be a Program instance", () => {
-    expect(Program).toEqual(ProgramClass)
+    expect(program).toBeInstanceOf(Program)
   })
 })
