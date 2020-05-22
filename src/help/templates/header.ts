0064ccbfc1f59ec1896bb42a41c09510eeb1605e
Matthias Etienne <matt@allty.com>
2020-05-22 17:25:26 +0200

    docs: update snapshots

--- src/help/templates/header.ts
+++ src/help/templates/header.ts
@@ -6,12 +6,13 @@ import type { TemplateContext, Template } from "../types"
 
 export const header: Template = (ctx: TemplateContext) => {
   const { prog, chalk: c, spaces, eol, eol2 } = ctx
+  const version = process.env?.NODE_ENV === "test" ? "" : prog.getVersion()
   return (
     eol +
     spaces +
     (prog.getName() || prog.getBin()) +
     " " +
-    (prog.getVersion() || "") +
+    (version || "") +
     (prog.getDescription() ? " \u2014 " + c.dim(prog.getDescription()) : "") +
     eol2
   )
