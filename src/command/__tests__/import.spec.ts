fa488575b89a402ba5496e76e42cbbf19d929b3b
Matthias Etienne <matt@allty.com>
2020-06-13 13:11:41 +0200

    fix(command): Fix importCommand() to handle both typescript or javascript files

--- src/command/__tests__/import.spec.ts
+++ src/command/__tests__/import.spec.ts
@@ -1,9 +1,12 @@
 import { importCommand } from "../import"
+import path from "path"
 
 describe("Command", () => {
   describe("importCommand()", () => {
     it("should import a command from a file", async () => {
-      const creator = await importCommand("./__fixtures__/example-cmd.ts")
+      const creator = await importCommand(
+        path.resolve(__dirname, "../__fixtures__/example-cmd.ts"),
+      )
       expect(creator).toBeTruthy()
     })
   })
--- src/command/import.ts
+++ src/command/import.ts
@@ -3,8 +3,11 @@
  * @internal
  */
 import { CommandCreator } from "../types"
+import path from "path"
 
 export async function importCommand(file: string): Promise<CommandCreator> {
-  const mod = await import(file)
+  const { dir, base } = path.parse(file)
+  const filename = path.join(dir, base)
+  const mod = await import(filename)
   return mod.default
 }
