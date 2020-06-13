47cdb9ce56b76ab9a79228b883d94b40f87b384f
Matthias Etienne <matt@allty.com>
2020-06-13 14:36:10 +0200

    fix(command): Fix command discovery
    Make webpack ignoring dynamic require

--- src/help/templates/program.ts
+++ src/help/templates/program.ts
@@ -7,7 +7,7 @@ import { getOptionsTable, getCommandsTable } from "../utils"
 
 export const program: Template = async (ctx: TemplateContext) => {
   const { prog, globalOptions, eol, eol3, colorize, tpl } = ctx
-  const commands = prog.getCommands()
+  const commands = await prog.getAllCommands()
   const options = Array.from(globalOptions.keys())
   const help =
     (await prog.getSynopsis()) +
