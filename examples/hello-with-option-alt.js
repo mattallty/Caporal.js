ce3e3750f3a0bdbcf7788ecdc2e648f16bba45e4
Zeeshan Ahmad <ziishaned@gmail.com>
2020-11-29 11:50:09 +0400

    Fix typo (#1)
    * Fix typo
    
    * Fix typo

--- examples/hello-with-option-alt.js
+++ examples/hello-with-option-alt.js
@@ -3,11 +3,11 @@ const { program } = require("@caporal/core")
 program
   .argument("<name>", "Name to greet")
   // we will be able to use either `-g` or `--greeting` in the command line
-  .option("-g, --greating <word>", "Greating to use", {
+  .option("-g, --greeting <word>", "Greeting to use", {
     default: "Hello",
   })
   .action(({ logger, args, options }) =>
-    logger.info("%s, %s!", options.greating, args.name),
+    logger.info("%s, %s!", options.greeting, args.name),
   )
 
 program.run()
